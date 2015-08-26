8669b353df56a126aae8bf00d5752a5786e1661d
Tamas Toth <tommey@tommey.hu>
2015-08-26 07:56:23 -0400

    Fix API methods to use the Product model properly

--- src/router.js
+++ src/router.js
@@ -6,27 +6,29 @@ var Product = require('./models/product');
 var router = Router();
 
 router.get('/product/:id', function* (next) {
-	this.body = yield User.find({_id: this.params.id});
+	this.body = yield Product.findById(this.params.id);
 });
 
 router.post('/product', function* (next) {
-        var product = new Product({
+        this.body = yield Product.create({
 		name: this.request.body.name,
 		color: this.request.body.color,
 		price: this.request.body.price
 	});
-
-	this.body = yield product.save();
 });
 
 router.del('/product/:id', function* (next) {
-        yield Product.remove({id: this.params.id});
+        yield Product.destroy({
+		where: {
+			id: this.params.id
+		}
+	});
 
 	this.status = 204;
 });
 
 router.get('/products', function* (next) {
-	this.body = yield Product.find();
+	this.body = yield Product.findAll();
 });
 
 module.exports = router;
